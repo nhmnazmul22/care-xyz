eabef5107ced2e481c8af9684ad8ef7208fad3a7
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-12 22:12:08 +0600

    update root layout to give min height and toaster for toast message

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,10 +1,13 @@
-import { Geist, Geist_Mono, Plus_Jakarta_Sans } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar/Navbar";
 import { ThemeProvider } from "@/provider/ThemeProvider";
+import Footer from "@/components/layout/Footer/Footer";
+import { Toaster } from "sonner";
 
 const PlusJakarta = Plus_Jakarta_Sans({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
+  subsets: ["latin"],
 });
 
 export const metadata = {
@@ -23,8 +26,10 @@ export default function RootLayout({ children }) {
           disableTransitionOnChange
         >
           <Navbar />
-          {children}
+          <main className="min-h-[calc(100vh-150px)] h-full">{children}</main>
+          <Footer />
         </ThemeProvider>
+        <Toaster />
       </body>
     </html>
   );
