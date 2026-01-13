da997a43b386c46755476cf86e5b57a22d80bea3
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-13 22:21:45 +0600

    update content of service page header

--- src/app/(root)/services/page.jsx
+++ src/app/(root)/services/page.jsx
@@ -1,13 +1,16 @@
 import PageHeader from "@/components/common/Header/PageHeader";
 import FilterSec from "@/components/pages/Services/FilterSec";
 import Services from "@/components/pages/Services/Services";
+import { BaggageClaim } from "lucide-react";
 
 const ServicesPage = () => {
   return (
     <>
       <PageHeader
+        badgeLabel="Our Services"
+        badgeIcon={<BaggageClaim className="w-4 h-4" />}
         title="Our Services"
-        subTitle="Browse our comprehensive range of professional caregiving services
+        description="Browse our comprehensive range of professional caregiving services
             tailored to your needs"
       />
       <FilterSec />
