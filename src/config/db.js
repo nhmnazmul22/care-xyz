439e1e0f614b09aa97d3086bc5e62555fff8da4d
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-14 22:42:23 +0600

    mongodb connection done

---
+++ src/config/db.js
@@ -0,0 +1,28 @@
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let cachedClient = null;
+let cachedDb = null;
+
+export const connectDB = async () => {
+  if (cachedClient && cachedDb) return cachedDb;
+
+  const client = new MongoClient(process.env.DATABASE_URL, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+
+  await client.connect();
+  const db = client.db(process.env.DATABASE_NAME);
+
+  cachedClient = client;
+  cachedDb = db;
+  return db;
+};
+
+export const collections = {
+  SERVICES: async () => (await connectDB()).collection("services"),
+  USERS: async () => (await connectDB()).collection("users"),
+};
