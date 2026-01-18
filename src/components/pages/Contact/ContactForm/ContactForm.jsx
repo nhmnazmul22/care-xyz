7c16520eae7bd0add334c7c54a1d7b26ccd28162
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-18 22:18:53 +0600

    Refactor the faq section in contact page

--- src/components/pages/Contact/ContactForm/ContactForm.jsx
+++ src/components/pages/Contact/ContactForm/ContactForm.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, {useState} from 'react';
 import {Label} from "@/components/ui/label.jsx";
 import {Input} from "@/components/ui/input.jsx";
--- src/components/pages/Contact/Faq/FaqSec.jsx
+++ src/components/pages/Contact/Faq/FaqSec.jsx
@@ -1,31 +1,41 @@
 import React from 'react';
 import {faqs} from "@/constant/index.js";
 import MotionContainer from "@/components/common/motion/MotionContainer.jsx";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion.jsx";
 
 const FaqSec = () => {
     return (
-        <div>
+        <div className="container-custom py-12">
             <h3 className="text-xl font-bold text-foreground mb-4">
                 Frequently Asked Questions
             </h3>
-            <div className="space-y-4">
-                {faqs.map((faq, index) => (
-                    <MotionContainer
-                        key={index}
-                        initial={{opacity: 0, y: 10}}
-                        whileInView={{opacity: 1, y: 0}}
-                        viewport={{once: true}}
-                        transition={{delay: index * 0.1}}
-                        className="bg-muted/50 p-4 rounded-lg"
-                    >
-                        <h4 className="font-medium text-foreground mb-2">
-                            {faq.question}
-                        </h4>
-                        <p className="text-sm text-muted-foreground">
-                            {faq.answer}
-                        </p>
-                    </MotionContainer>
-                ))}
+            <div className="">
+                <Accordion
+                    type="single"
+                    collapsible
+                    className="w-full space-y-4"
+                    defaultValue="item-1"
+                >
+                    {faqs.map((faq, index) => (
+                        <MotionContainer
+                            key={index}
+                            initial={{opacity: 0, y: 10}}
+                            whileInView={{opacity: 1, y: 0}}
+                            viewport={{once: true}}
+                            transition={{delay: index * 0.1}}
+                            className="bg-muted/50 p-4 rounded-lg"
+                        >
+                            <AccordionItem value={`item-${index + 1}`}>
+                                <AccordionTrigger> {faq.question}</AccordionTrigger>
+                                <AccordionContent className="flex flex-col gap-4 text-balance">
+                                    {faq.answer}
+                                </AccordionContent>
+                            </AccordionItem>
+                        </MotionContainer>
+                    ))}
+
+                </Accordion>
+
             </div>
         </div>
     );
