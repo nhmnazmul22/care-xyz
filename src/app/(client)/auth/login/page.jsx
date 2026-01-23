7ca8df6eb6f2ab6df6079623c68278d8690d35f5
nhmnazmul22 <najmulmonir12345@gmail.com>
2026-01-23 22:24:59 +0600

    fix suspense issue

--- src/app/(client)/auth/login/page.jsx
+++ src/app/(client)/auth/login/page.jsx
@@ -1,19 +1,22 @@
 import LoginPageHeader from "@/components/pages/Login/LoginPageHeader";
 import LoginForm from "@/components/pages/Login/LoginForm";
+import {Suspense} from "react";
 
 const LoginPage = () => {
-  return (
-    <div>
-      <section className="pt-32 pb-20 min-h-screen flex items-center">
-        <div className="container-custom">
-          <div className="max-w-lg mx-auto">
-            <LoginPageHeader />
-            <LoginForm />
-          </div>
+    return (
+        <div>
+            <section className="pt-32 pb-20 min-h-screen flex items-center">
+                <div className="container-custom">
+                    <div className="max-w-lg mx-auto">
+                        <LoginPageHeader/>
+                        <Suspense fallback={null}>
+                            <LoginForm/>
+                        </Suspense>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default LoginPage;
